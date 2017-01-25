8a9248b64dc78115ab059c630afdcf4426633c43
Braden Pezeshki <braden.pezeshki@gmail.com>
2017-01-24 21:22:30 -0800

    hey it is again

--- srcs/file_len.c
+++ srcs/file_len.c
@@ -8,17 +8,11 @@ int		file_len(const char *path)
 	int		status;
 
 	fd = open(path, O_RDONLY);
-	if (fd == -1 || (status = read(fd, &buf, 1)) < 0)
-	{
+	if (fd == -1 || (status = read(fd, &buf, 0)) < 0)
 		return (-1);
-	}
-	close(fd);
-	fd = open(path, O_RDONLY);
 	len = 0;
 	while ((status = read(fd, &buf, 1)) > 0)
-	{
 		len++;
-	}
 	close(fd);
 	return (status != 0 ? -1 : len);
 }
